dc36358521a2d2d5e9c3b8d1b89b3ad49ce224da
Jon Barson <Jon Barson>
2021-11-23 17:01:15 -0500

    fixed post demo

--- expressServer.js
+++ expressServer.js
@@ -4,7 +4,8 @@ const port = 8081
 
 const app = express()
 
-app.use(express.json())
+// app.use(express.json())
+app.use(express.urlencoded())
 
 app.set("view engine", "ejs")
 app.set("views", "views")
@@ -28,11 +29,13 @@ app.get('/shows', (req, res) => {
 })
 
 app.post('/shows', (req, res) => {
-  console.log(req)
+  console.log(req.body.show)
+  shows.push(req.body.show)
   res.render('shows', { shows: shows })
 })
 
 app.get('/about', (req, res) => {
+
   res.send('<h1>About time for express!</h1>')
 })
 
